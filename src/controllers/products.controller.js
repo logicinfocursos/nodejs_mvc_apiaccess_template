5c4ce5b29bb2656f4b14094f1838cb8b6f94aee9
logicinfo <116040839+logicinfocursos@users.noreply.github.com>
2024-06-16 11:02:27 -0300

    Update products.controller.js

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,57 +1,49 @@
 // src\controllers\home.controller.js - logicinfo.com.br
 import ProductsRepository from '../models/repositories/products.repository.js'
 
+class ProductsController {
+    constructor() {
+        this.productsRepository = new ProductsRepository()
+    }
 
+    list = async (req, res) => {
+        const { data: products } = await this.productsRepository.getAll()
 
-const list = async (req, res) => {
-    const productsRepository = new ProductsRepository();
-    const { data: products } = await productsRepository.getAll();
+        const data = {
+            products,
+            title:  "lista de produtos",        
+        } 
 
-    const data = {
-        products,
-        title:  "lista de produtos",        
-    } 
+        res.render('products/index.ejs', data)
+    }
 
-    res.render('products/index.ejs', data);
+    view = async (req, res) => {
+        const { id } = req.params
+        const result = await this.productsRepository.getById(id)
+        const product = result.data[0]
+
+        const data = {
+            product,
+            title:  `product: ${product.name.substring(0, 30)}`,        
+        } 
+
+        res.render('products/view.ejs', data)
+    }
+
+    add = async (req, res) => {
+        const data = {
+            title:   "adicionar produto",        
+        } 
+
+        res.render('products/add.ejs', data)
+    }
+
+    save = async (req, res) => {
+        const { name, details } = req.body
+        const result = await this.productsRepository.create(req.body)
+
+        res.redirect('/products') 
+    }
 }
 
-
-
-const view = async (req, res) => {
-    const { id } = req.params;
-    const productsRepository = new ProductsRepository();
-    const result = await productsRepository.getById(id);
-    const product = result.data[0];
-
-    const data = {
-        product,
-        title:  `product: ${product.name.substring(0, 30)}`,        
-    } 
-
-    res.render('products/view.ejs', data);
-}
-
-
-
-const add = async (req, res) => {
-    
-    const data = {
-        title:   "adicionar produto",        
-    } 
-
-    res.render('products/add.ejs', data);
-}
-
-
-
-const save = async (req, res) => {
-    const { name, details } = req.body;
-    const productsRepository = new ProductsRepository();
-    const result = await productsRepository.create(req.body);
-
-    res.redirect('/products'); 
-}
-
-export default {
-    list, view, add, save
-}
+export default new ProductsController()
