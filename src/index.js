50c473a1032b78e9ca89afc02c3a3794f246daab
logicinfo <116040839+logicinfocursos@users.noreply.github.com>
2024-06-16 11:02:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
 // src\index.js
+// Importando os módulos necessários
 import express from 'express'
-import bodyParser from 'body-parser'
+import { urlencoded } from 'express'
 import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
 import path from 'path'
 import router from './routes/router.js'
 
+// Criando uma função require para importar módulos CommonJS
 const require = createRequire(import.meta.url)
+
+// Importando e configurando o módulo dotenv para gerenciar variáveis de ambiente
 require('dotenv').config()
 
-// Get the current directory name
+// Obtendo o diretório do arquivo atual
 const dirname = path.dirname(fileURLToPath(import.meta.url))
 
+// Definindo a porta do servidor a partir das variáveis de ambiente ou usando 3111 como padrão
 const port = process.env.PORT || 3111
 
+// Criando uma instância do aplicativo Express
 const app = express()
 
+// Definindo EJS como a engine de visualização
 app.set('view engine', 'ejs')
+
+// Definindo o diretório de arquivos estáticos
 app.use(express.static(path.join(dirname, 'public')))
 
-// Set the views directory
+// Definindo o diretório das views
 app.set('views', path.join(dirname, 'views')) 
 
-app.use(bodyParser.urlencoded({ extended: true }))
-// define /routes/router.js para distribuir as rotas da aplicação
+// Configurando o middleware para analisar o corpo das requisições
+app.use(urlencoded({ extended: true }))
+
+// Usando o roteador importado
 app.use(router)
 
+// Iniciando o servidor na porta definida
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
